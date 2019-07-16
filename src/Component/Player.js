e68862efeb8808e1ba4d9430de8a524038051b88
Subash Pradahan <suprowilde5@gmail.com>
2019-07-16 14:44:39 +0200

    making button work

--- src/Component/Player.js
+++ src/Component/Player.js
@@ -1,16 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-class Player extends Component{
+class Player extends Component {
     static propTypes = {
-        name : PropTypes.string.isRequired
-    } 
-    render(){
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        score: PropTypes.number.isRequired,
+        incrementScore: PropTypes.func.isRequired
+    }
+    handleClick = () => {
+        this.props.incrementScore(this.props.id);
+
+    }
+    render() {
         return (
             <div className="player">
                 {/* <p className="id">{this.props.id}</p> */}
-                <p className ="name">Name: {this.props.name} </p>
+                <p className="name">Name: {this.props.name} </p>
                 <p className="scores">Score: {this.props.score}</p>
+                <button className="plus-button" onClick={this.handleClick}>+</button>
             </div>
         )
     }
--- src/Component/Scoreboard.js
+++ src/Component/Scoreboard.js
@@ -2,36 +2,50 @@ import React, { Component } from 'react'
 import Player from './Player'
 
 class Scoreboard extends Component {
-    state = { 
-         players: [
-            {id:1, name:"Subash", score:5},
-            {id:2, name:"Mimi", score:3},
-            {id:3, name:"Kelley", score:4}
-            ]
+    state = {
+        players: [
+            { id: 1, name: "Subash", score: 5 },
+            { id: 2, name: "Mimi", score: 3 },
+            { id: 3, name: "Kelley", score: 4 }
+        ]
     }
-    render(){
+    render() {
         const players_copy = [...this.state.players]
-        players_copy.sort((a,b) => b.score - a.score)
-        return(
-            
-            <div className ="scoreboard">
+        players_copy.sort((a, b) => b.score - a.score)
+        return (
+
+            <div className="scoreboard">
                 <h2>ScoreBoard</h2>
                 {players_copy.map(this.renderPlayer)}
-                
+
             </div>
         )
     }
-    renderPlayer = player =>{
+    renderPlayer = player => {
         // console.log(player)
-        return(
+        return (
             <Player
-                id = {player.id}
-                name = {player.name}            
-                score = {player.score}
-                key = {player.id}
+                id={player.id}
+                name={player.name}
+                score={player.score}
+                key={player.id}
+                incrementScore={this.incrementScoreOfPlayer}
             />
-        )}
-}
+        )
+    }
+    incrementScoreOfPlayer = (id) => {
+        const updatedPlayers = this.state.players.map((player) => {
+            if(player.id === id){
+                console.log(player)
+                return {player, score: player.score + 1}
+            }else{
+                return player
+            }
+        })
+        this.setState({players : updatedPlayers})
+    }
+      
+    }
 
 
 
