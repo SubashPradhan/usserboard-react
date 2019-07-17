49872c49c4a3d799b64d314d6b866a2150c303fe
Subash Pradahan <suprowilde5@gmail.com>
2019-07-17 11:24:33 +0200

    Making sure everything is commited

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import Title from './Component/Title'
 // import Player from './Component/Player'
 import Scoreboard from './Component/Scoreboard'
